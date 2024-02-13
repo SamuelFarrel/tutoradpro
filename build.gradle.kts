6e4c227c2f576d72e8930aa0b964c529565cff1f
SamuelFarrel <samuel.farrel@ui.ac.id>
2024-02-13 23:43:29 +0700

    update sonarcloud

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     jacoco
     id("org.springframework.boot") version "3.2.2"
     id("io.spring.dependency-management") version "1.1.4"
-    id("org.sonarqube") version "3.5.0.2730"
+    id("org.sonarqube") version "4.4.1.3373"
 }
 
 group = "id.ac.ui.cs.advprog"
@@ -78,5 +78,13 @@ tasks.jacocoTestReport{
     dependsOn(tasks.test)
     reports{
         xml.required = true
+    }
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "SamuelFarrel_tutoradpro")
+        property("sonar.organization", "samuelfarrel")
+        property("sonar.host.url", "https://sonarcloud.io")
     }
 }
